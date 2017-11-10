d1c61bd823ff26dd3e4e699143227d26b0ff804e
peggy10515101 <32382265+peggy10515101@users.noreply.github.com>
2017-11-10 14:36:36 +0800

    123

--- httpd/index.js
+++ httpd/index.js
@@ -7,11 +7,11 @@ const routingTable = {
     url: '../htdocs/index.html',
     type: 'text/html'
   },
-  '/styles.css': {
+  '/assets/css/styles.css': {
     url: '../htdocs/assets/css/styles.css',
     type: 'text/css'
   },
-  '/SokobanClone_byVellidragon.png': {
+  '/assets/png/SokobanClone_byVellidragon.png': {
     url: '../htdocs/assets/png/SokobanClone_byVellidragon.png',
     type: 'image/png'
   },
