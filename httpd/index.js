3325fd11507e7f1f326fb1e2c444f77f3d9ed5dd
peggy10515101 <32382265+peggy10515101@users.noreply.github.com>
2017-11-10 13:30:22 +0800

    123

--- httpd/index.js
+++ httpd/index.js
@@ -2,13 +2,57 @@
 
 let http = require('http');
 
+const routingTable = {
+'/': {
+url: '../htdocs/index.html',
+type: 'text/html'
+},
+'/styles.css': {
+url: '../htdocs/assets/css/styles.css',
+type: 'text/css'
+},
+'/SokobanClone_byVellidragon.png': {
+url: '../htdocs/assets/png/SokobanClone_byVellidragon.png',
+type: 'text/css'
+},
+};
+
+ /**
+ * 利⽤ http.ServerResponse 物件回傳檔案內容
+ *
+ * @name serve
+ * @function
+ * @param response - http.ServerResponse 物件
+ * @param fname - 要回傳的檔案名
+ * @param datatype - 回傳檔案內容的 Mine-Type
+ * @returns {undefined}
+ */
+let serve = (response, fname, datatype) => {
+let fs = require('fs');
+
+fs.readFile(fname, (err, data) => {
+if (err) {
+console.log(' 檔案讀取錯誤');
+}
+else {
+response.writeHead(200, {
+'Content-Type': datatype
+});
+
+response.write(data);
+response.end();
+}
+});
+};
+
 http.createServer((request, response) => {
 let fs = require('fs');
-let postData = ''; // POST 資料
+
+let postData = '';
 
 // 利⽤ 'data' event 消耗掉 data chunk;
 // 'end' event 才會被 fired
-request.on('data', (chunk) =>
+request.on('data', (chunk) => {
 postData += chunk;
 
 console.log(
@@ -17,33 +61,20 @@ console.log(
 });
 
 request.on('end', () => {
-switch (request.url) {
-case '/':
-fs.readFile('../htdocs/index.html', (err, data) => {
-if (err) {
-console.log(' 檔案讀取錯誤');
+if (request.url in routingTable) {
+let obj = routingTable[request.url];
+
+serve(response, obj.url, obj.type);
 }
 else {
-response.writeHead(200, {
-'Content-Type': 'text/html'
-});
-
-response.write(data);
-response.end();
-}
-});
-
-break;
-
-default:
 console.log(' 未定義的存取: ' + request.url);
 
 response.end();
-
-break;
 }
 });
 }).listen(8088);
 
 // log message to Console
 console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
+
+// index.js
