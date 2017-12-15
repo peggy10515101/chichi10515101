2188f3d691a9e69e9450c86340f5aee85d7ca176
peggy10515101 <pinkypeggy123@gmail.com>
2017-12-15 15:40:08 +0800

    123

--- test/index.js
+++ test/index.js
@@ -5,7 +5,12 @@ console.log("index.js loaded");
 let h1 = document.createElement('h1');
 let msg = document.createTextNode('Hello');
 
+let h2 = document.createElement('h2');
+let msgl = document.createTextNode('Hello');
+
 h1.appendChild(msg);
+h2.appendChild(msgl);
 
 document.body.appendChild(h1);
+document.body.appendChild(h2);
 });
