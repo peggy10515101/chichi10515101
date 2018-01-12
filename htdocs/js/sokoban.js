f6b2ffcbca9ea8a072c9a7afd54ac6ac7563e8b1
peggy10515101 <pinkypeggy123@gmail.com>
2018-01-12 14:32:11 +0800

    123

--- htdocs/js/sokoban.js
+++ htdocs/js/sokoban.js
@@ -237,7 +237,7 @@ let prototypeGameState = {
   moveBoxIn: function (cell) {
     if (this.isGoal(cell)) {
       this.putBoxOnGoal(cell);
-      if (putBoxOnGoal(cell)!=NULL)
+      if (this!=NULL)
        {
          alert ( "you win !!!" );
        }
